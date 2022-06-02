40f58fbe262c47c52d97a6342ef74274c6a23fd0
te6-in <me@te6.in>
2022-06-02 20:43:59 +0900

    some updates

--- maps/script.js
+++ maps/script.js
@@ -1,11 +1,18 @@
-window.onload = function() {
-	document.body.className = "";
-}
+window.addEventListener("load", () => {
+	document.body.classList.remove("preload");
+});
 
 function setThemeColor() {
-	document.querySelector('meta[name="theme-color"]').setAttribute("content", getComputedStyle(document.body).getPropertyValue("--background"));
+	document
+		.querySelector('meta[name="theme-color"]')
+		.setAttribute(
+			"content",
+			getComputedStyle(document.body).getPropertyValue("--background")
+		);
 }
 
 setThemeColor();
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", setThemeColor);
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", setThemeColor);
